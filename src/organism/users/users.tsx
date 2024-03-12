e062726ea56e02963c774af2ad7f6e7deeae80d9
quynhdtd-2775 <doan.thi.diem.quynh@sun-asterisk.com>
2024-03-12 23:59:01 +0700

    Create user

--- src/organism/users/users.tsx
+++ src/organism/users/users.tsx
@@ -1,8 +1,13 @@
 "use client";
 import { IUser } from "@/types/backend";
-import { Table } from "antd";
+import { Table, Button, Popconfirm } from "antd";
 import { ColumnsType } from "antd/es/table";
 import { usePathname, useSearchParams, useRouter } from "next/navigation";
+import { EditTwoTone, PlusOutlined, DeleteTwoTone } from "@ant-design/icons";
+import CreateUserModal from "./createUserModal";
+import { useState } from "react";
+import { handleDeleteUserAction } from "@/actions";
+import UpdateUser from "./updateUserModal";
 
 interface IProps {
   users: IUser[] | [];
@@ -14,7 +19,10 @@ const UserTable = (props: IProps) => {
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  console.log(users);
+  const [isCreateModalOpen, setIsCreateModalOpen] = useState<boolean>(false);
+  const [isUpdateModalOpen, setIsUpdateModalOpen] = useState<boolean>(false);
+  const [dataUpdate, setDataUpdate] = useState<any>(null);
+
   const columns: ColumnsType<IUser> = [
     {
       title: "Id",
@@ -31,16 +39,55 @@ const UserTable = (props: IProps) => {
     {
       title: "Action",
       dataIndex: "action",
+      width: "10%",
       render: (value, record, index) => {
         return (
-          <>
-            <div> Edit </div>
-            <div> Delete </div>
-          </>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <EditTwoTone
+              twoToneColor="#f57800"
+              style={{ cursor: "pointer", margin: "0 20px" }}
+              onClick={() => {
+                setIsUpdateModalOpen(true);
+                setDataUpdate(record);
+              }}
+            />
+
+            <Popconfirm
+              placement="leftTop"
+              title={"Xác nhận xóa user"}
+              description={"Bạn có chắc chắn muốn xóa user này ?"}
+              onConfirm={() => handleDeleteUser(record)}
+              okText="Xác nhận"
+              cancelText="Hủy"
+            >
+              <span style={{ cursor: "pointer" }}>
+                <DeleteTwoTone twoToneColor="#ff4d4f" />
+              </span>
+            </Popconfirm>
+          </div>
         );
       },
     },
   ];
+
+  const handleDeleteUser = async (user: any) => {
+    await handleDeleteUserAction({ id: user.id });
+  };
+
+  const renderHeader = () => {
+    return (
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <span>Table List Users</span>
+        <Button
+          icon={<PlusOutlined />}
+          type="primary"
+          onClick={() => setIsCreateModalOpen(true)}
+        >
+          Add a new users
+        </Button>
+      </div>
+    );
+  };
 
   const onChange = (pagination: any, filter: any, sorter: any, extra: any) => {
     if (pagination && pagination.current) {
@@ -53,6 +100,7 @@ const UserTable = (props: IProps) => {
   return (
     <div>
       <Table
+        title={renderHeader}
         rowKey={"id"}
         bordered
         dataSource={users}
@@ -63,6 +111,18 @@ const UserTable = (props: IProps) => {
           total: 20,
         }}
         onChange={onChange}
+      />
+
+      <CreateUserModal
+        isCreateModalOpen={isCreateModalOpen}
+        setIsCreateModalOpen={setIsCreateModalOpen}
+      />
+
+      <UpdateUser
+        isUpdateModalOpen={isUpdateModalOpen}
+        setIsUpdateModalOpen={setIsUpdateModalOpen}
+        dataUpdate={dataUpdate}
+        setDataUpdate={setDataUpdate}
       />
     </div>
   );
